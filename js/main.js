c998329e9bce309c12567f6bbb2d8636457b7681
Vale L <106398144+vlysi@users.noreply.github.com>
2024-08-23 22:36:49 -0300

    Add project and contact sections

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
-if (navToggle) {
+if(navToggle) {
   navToggle.addEventListener('click', () => {
     navMenu.classList.add('show-menu')
   })
 }
 
-if (navClose) {
+if(navClose){
   navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu')
   })
@@ -26,3 +26,32 @@ const linkAction = () => {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+/*sombra header*/
+const shadowHeader = () => {
+  const header = document.getElementById('header')
+  this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader);
+
+/* email*/
+const contactForm = document.getElementById('contact-form');
+const contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+  e.preventDefault()
+
+  emailjs.sendForm('service_n158azb','template_f808n7c','#contact-form','fjCJoZsxrswEtLF1Y')
+  .then(() => {
+    contactMessage.textContent = 'Message sent successfully'
+
+    setTimeout(() => {
+      contactMessage.textContent = ''
+    }, 5000)
+
+    contactForm.reset();
+  }, () => {
+    contactMessage.textContent = 'Message not sent (service error)'
+  })
+}
+
+contactForm.addEventListener('submit', sendEmail)
